afb81037a8ce345e187c89c5819b8a0e8df17a44
shashi <shashikant.gurav@gmail.com>
2016-09-07 17:57:37 +0100

    Added Item class Attribute and impl of checkItem method

--- src/main/java/item/Item.java
+++ src/main/java/item/Item.java
@@ -1,7 +1,37 @@
 package item;
 
+import java.math.BigDecimal;
+
 /**
  * Created by sony on 9/7/2016.
  */
 public class Item {
+
+    private String fruitName ;
+    private int quantity ;
+    private BigDecimal price ;
+
+    public String getFruitName() {
+        return fruitName;
+    }
+
+    public void setFruitName(String fruitName) {
+        this.fruitName = fruitName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
 }
--- src/main/java/shopping/ShoppingCartImpl.java
+++ src/main/java/shopping/ShoppingCartImpl.java
@@ -2,17 +2,46 @@ package shopping;
 
 import item.Item;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by sony on 9/7/2016.
  */
 public class ShoppingCartImpl implements ShoppingCart{
+
+    private BigDecimal finalBill = new BigDecimal(BigInteger.ZERO);
+
+    private Map<String,Item> itemCart ;
+
+    /*
+        initialise Shopping cart
+     */
+    public ShoppingCartImpl (){
+        itemCart = new HashMap<String, Item>();
+    }
+
+    /*
+        Add the List of item in shopping cart as per req.
+     */
     public void addItems(List<Item> itemList) {
 
     }
-
+/*
+    Check the number if Items in the cart
+ */
     public int checkItem() {
-        return 0;
+        int count=0 ;
+
+        Iterator<Item> itemIterator = itemCart.values().iterator();
+        while (itemIterator.hasNext()){
+            count=count+itemIterator.next().getQuantity();
+        }
+
+        return count;
     }
 }
